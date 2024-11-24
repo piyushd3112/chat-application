018c49684189649d04cfd800198c3e46915c072a
piyushd3112 <dhonglep@gmail.com>
2024-11-25 00:45:01 +0530

    Created models

---
+++ backend/models/messageModel.js
@@ -0,0 +1,23 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const messageModel = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = Mongoose.model("Message", messageModel);
